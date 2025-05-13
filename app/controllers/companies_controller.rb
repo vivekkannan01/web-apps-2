a5761c9c82c92b3fb919674fc1fe92880bc5d8b7
Vivek Kannan <vivek.kannan@kellogg.northwestern.edu>
2025-05-13 16:32:09 +0000

    in-class exercise

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,24 +2,40 @@ class CompaniesController < ApplicationController
 
   def index
     # find all Company rows
+    @companies = Company.all
     # render companies/index view
   end
 
   def show
     # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    @contacts = Contact.where({"company_id" => @company["id"]})
+    puts @company
     # render companies/show view with details about Company
   end
 
   def new
     # render view with new Company form
+    @company = Company.find_by({"id"})
   end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    
+    # start with a new Company
+    @company = Company.new
+    
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+   
+    # save Company row
+    @company.save
+
+    # redirect user
+    redirect_to "/companies"
+
+  end
 
   # def edit
   #   # find a Company
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,21 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
+  def create
   #   # start with a new Post
   #   # assign user-entered form data to Post's columns
   #   # save Post row
   #   # redirect user
-  # end
+
+  @post = Post.new
+  @post["author"] = params["author"]
+  @post["image"] = params["image"]
+  @post["body"] = params["body"]
+
+  @post.save
+
+  redirect_to "/posts"
+
+  end
 
 end
